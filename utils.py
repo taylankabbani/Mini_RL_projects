09af7fc2e6f10c7c7e90c079a2f2a9a35bf9ba23
taylankabbani <taylankabbani96@gmail.com>
2021-08-04 23:21:01 +0300

    Fix for toy games

--- utils.py
+++ utils.py
@@ -2,15 +2,13 @@ import gym
 import regex as re
 
 
-def available_env(Agent):
+def available_env(Agent='Atari'):
     """Show available environment"""
     envs_list = []
-    if Agent == 'Dumb':
-        pattern = r'(?i)[A-Z0-9-]+.(?=\))'
-    elif Agent == 'QLearning':
-        pass
-    elif Agent == 'Dqn':
-        pass
+    pattern = r'(?i)[A-Z0-9-]+.(?=\))'
+    if Agent == 'toy':
+        envs_list = ["FrozenLake-v0", "FrozenLake8x8-v0", "CliffWalking-v0", "NChain-v0", "Roulette-v0", "Taxi-v3"]
+        return envs_list
     for game in gym.envs.registry.all():
         env_name = re.search(pattern, str(game))
         if env_name is None:
